16d5be6833e6adc23bd2ad95ee9058e648b2ce70
bridgerdanby <118231330+bridgerdanby@users.noreply.github.com>
2024-02-12 14:40:16 -0700

    Supabase test (#5)
    * added supabase to reqs
    
    * connected to supabase
    
    * added a function to add a user to the database
    
    * get_user_by_id and delete_user_by_id
    
    * get_store_employess and get_store_admins
    
    * Refactored into unit tests. First three are done
    
    * added test to get employees and admins from a store
    
    * added a test for banned person
    adding, removing, looking up, looking up by store and getting all
    
    * lint cleanup
    
    * Added ability to get all images of a person, add images of a person and remove them.
    Created a unit test as well
    
    * linting
    
    * Added a function to update a banned_person's information
    There is also a unit test for it
    
    * Added models to reduce number of parameters
    Unit tests dont work quite yet
    
    * migrated teest_ban_person to use models
    
    * migrated test_banned_person_images to use models
    
    * finished creating functions and writing tests
    
    * swappped to a .env file

---
+++ models/store.py
@@ -0,0 +1,16 @@
+"""
+A model for a store that mimics the database schema
+"""
+
+class Store:
+    """
+    A store that has cameras installed
+    """
+    def __init__(self, name, address, billing_info, id=None):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.billing_info = billing_info
+
+    def __str__(self):
+        return f"Store: {self.name} {self.address} {self.billing_info} {self.id}"
