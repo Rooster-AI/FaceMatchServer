c87152b7b4cebe0ef5a108e2cc624f5bfaf27f0c
antonalley <anton.alley@gmail.com>
2024-02-23 13:43:24 -0700

    git keep auth note, git pull bash

--- restart_docker.sh
+++ restart_docker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Get the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Change to the directory of the script
+cd "$DIR"
 
 # Stop all docker containers
 docker stop $(docker ps -q)
+
+git pull
 
 # Rebuild
 docker build -t rooster-server .
