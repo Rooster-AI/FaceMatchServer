f48a0fb8666851c65fff224fc64b3b6b15769cfb
antonalley <anton.alley@gmail.com>
2024-02-23 13:12:59 -0700

    rebuild in bash file

--- restart_docker.sh
+++ restart_docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
 # Stop all docker containers
 docker stop $(docker ps -q)
+
+# Rebuild
+docker build -t rooster-server .
 
 # Start the Docker container
 docker run -d -p 5000:5000 --restart always rooster-server
